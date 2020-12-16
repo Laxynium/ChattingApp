90ed684eb086299da47a4f85c6bd78d4b245000e
Grzegorz Gruszczyk <grzesiu15432@gmail.com>
2020-12-16 19:02:24 +0100

    fix api url

--- src/InstantMessenger.Web/ClientApp/src/environments/environment.prod.ts
+++ src/InstantMessenger.Web/ClientApp/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.instantmessenger.azurewebsites.net/api',
+  apiUrl: 'https://api-instantmessenger.azurewebsites.net/api',
 };
