ab3c0edd9b45840b0f55ef4e9bce2acda10befb3
Grzegorz Gruszczyk <grzesiu15432@gmail.com>
2020-12-16 16:21:50 +0100

    Fix imports

--- src/InstantMessenger.Web/ClientApp/src/app/identity/store/actions/activate.actions.ts
+++ src/InstantMessenger.Web/ClientApp/src/app/identity/store/actions/activate.actions.ts
@@ -1,7 +1,6 @@
 import {createAction, props} from '@ngrx/store';
-
-import {ActionTypes} from '../actionTypes';
-import {ActivateRequestInterface} from '../../types/activateRequest.interface';
+import { ActionTypes } from 'src/app/identity/store/actionTypes';
+import { ActivateRequestInterface } from 'src/app/identity/types/activateRequest.interface';
 
 export const activateAction = createAction(
   ActionTypes.ACTIVATE,
