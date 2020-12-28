501272bd8ddad66f5717e6675b04dd22dd196ce7
Grzegorz Gruszczyk <grzesiu15432@gmail.com>
2020-12-28 13:45:52 +0100

    Add app insights

--- src/InstantMessenger.Api/Startup.cs
+++ src/InstantMessenger.Api/Startup.cs
@@ -17,6 +17,7 @@ namespace InstantMessenger.Api
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddApplicationInsightsTelemetry();
             services.AddControllers()
                 .AddControllersAsServices()
                 .AddNewtonsoftJson(options =>
